7651b268a769ea4beaa307872b433171672789d9
Matt Barry <matt.barry@cengage.com>
2019-11-20 13:53:11 -0500

    database allowed_sec_groups_count - no such arg

--- database.tf
+++ database.tf
@@ -17,7 +17,7 @@ module "database" {
   subnets = var.database_subnets
 
   allowed_security_groups       = [aws_security_group.ecs_service_egress.id]
-  allowed_security_groups_count = 1
+  #allowed_security_groups_count = 1
   instance_type                 = var.db_instance_type
   storage_encrypted             = true
   apply_immediately             = true
